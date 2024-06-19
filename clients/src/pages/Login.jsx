1070bf1cad3ede0b9ad8d1dd9ae82815b7279aa9
lebohangmdm <lebohangmdm@gmail.com>
2024-06-19 19:50:16 +0200

    add register and login functionality

--- clients/src/pages/Login.jsx
+++ clients/src/pages/Login.jsx
@@ -1,41 +1,79 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { LoginValidation } from "../utils/validation";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { InputError } from "../components";
+import { useLoginMutation } from "../services/authService";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import { handleFirstWord } from "../utils/helpers";
 
 const Login = () => {
+  const [loginUser, { isLoading, isError, error }] = useLoginMutation();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(LoginValidation),
+  });
+
+  const onSubmit = async (data) => {
+    const { data: dataInfo } = await loginUser(data);
+    if (dataInfo?.success) {
+      const firstName = handleFirstWord(dataInfo?.data.user.fullName);
+      toast.success(`Welcome back, ${firstName}`);
+    }
+  };
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(error.data.message);
+    }
+  }, [isError, error]);
+
   return (
     <section className="bg-banner">
-      <div className="w-full md:w-1/2 px-4 py-6 md:p-0 md:py-0 ">
+      <div className="w-full md:w-1/2 px-4 py-6 md:px-6 ">
         <div className="flex justify-center items-center h-screen">
-          <div className="max-w-md  w-full px-4 py-6  border-white border rounded-md shadow-md">
+          <div className="max-w-md w-full px-4 py-6 lg:py-8 lg:px-8 border-white border rounded-md shadow-md">
             <h1 className="text-2xl text-white font-semibold text-center mb-4">
               Login
             </h1>
-            <form action="" className="space-y-4">
+            <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
               <div className="flex flex-col space-y-2">
-                <label htmlFor="fullName" className="text-white md:text-lg">
+                <label htmlFor="fullName" className="text-white ">
                   Email:
                 </label>
                 <input
                   type="email"
                   name="email"
+                  {...register("email")}
                   placeholder="you@example.com"
-                  className="py-2 px-4 bg-transparent  text-white border border-light-1 md:text-lg rounded md:rounded-md"
+                  className="py-2 px-4 bg-transparent  text-white border border-light-1  rounded md:rounded-md"
                 />
+                {errors.email && <InputError error={errors.email.message} />}
               </div>
               <div className="flex flex-col space-y-2">
-                <label htmlFor="fullName" className="text-white md:text-lg">
+                <label htmlFor="fullName" className="text-white ">
                   Password:
                 </label>
                 <input
                   type="password"
                   name="password"
-                  className="py-2 px-4 bg-transparent  text-white border border-light-1 md:text-lg rounded md:rounded-md"
+                  {...register("password")}
+                  className="py-2 px-4 bg-transparent  text-white border border-light-1  rounded md:rounded-md"
                 />
+                {errors.password && (
+                  <InputError error={errors.password.message} />
+                )}
               </div>
               <button
                 type="submit"
-                className="w-full bg-light-3 text-white py-2 px-4 rounded-md hover:bg-black focus:outline-none focus:bg-black"
+                disabled={isLoading}
+                className="w-full bg-light-3 text-white py-2 px-4 rounded-md hover:bg-black focus:outline-none hover:border hover:border-white focus:ring ring-white"
               >
-                Login
+                {isLoading ? "Loading" : "Login"}
               </button>
               <p className="text-sm text-white text-center">
                 Already have an account?{" "}
--- clients/src/pages/Register.jsx
+++ clients/src/pages/Register.jsx
@@ -1,63 +1,106 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { RegisterValidation } from "../utils/validation";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { InputError } from "../components";
+import { useRegisterMutation } from "../services/authService";
+import { toast } from "react-hot-toast";
+import { useEffect } from "react";
+import { handleFirstWord } from "../utils/helpers";
 
 const Register = () => {
+  const [registerUser, { isLoading, isError, error }] = useRegisterMutation();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(RegisterValidation) });
+
+  const onSubmit = async (data) => {
+    const { data: dataInfo } = await registerUser(data);
+    if (dataInfo?.success) {
+      const firstName = handleFirstWord(dataInfo?.data.user.fullName);
+      toast.success(`Welcome, ${firstName}! Glad to have you join us! 🎉 `);
+    }
+  };
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(error.data.message);
+    }
+  }, [isError, error]);
+
   return (
     <section className="bg-banner">
-      <div className="w-full md:w-1/2 px-4 py-6 md:p-0 md:py-0 ">
+      <div className="w-full md:w-1/2 px-4 py-6 md:px-6 ">
         <div className="flex justify-center items-center min-h-screen ">
-          <div className="max-w-md  w-full py-4 px-6 md:py-8 border-white border rounded-md shadow-md">
+          <div className="max-w-md  w-full py-4 px-6 lg:py-8 lg:px-8 border-white border rounded-md shadow-md">
             <h1 className="text-2xl text-white text-center font-semibold mb-4">
               Register
             </h1>
-            <form action="" className="space-y-5">
+            <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
               <div className="flex flex-col space-y-1">
-                <label htmlFor="fullName" className="text-white md:text-lg">
+                <label htmlFor="fullName" className="text-white ">
                   Full Name:
                 </label>
                 <input
                   type="text"
                   name="fullName"
+                  {...register("fullName")}
                   placeholder="John Doe"
-                  className="py-2 px-4 bg-transparent  text-white border border-light-1 md:text-lg rounded md:rounded-md"
+                  className="py-2 px-4 bg-transparent  text-white border border-light-1  rounded md:rounded-md"
                 />
+                {errors.fullName && (
+                  <InputError error={errors.fullName.message} />
+                )}
               </div>
               <div className="flex flex-col space-y-2">
-                <label htmlFor="fullName" className="text-white md:text-lg">
+                <label htmlFor="fullName" className="text-white ">
                   Email:
                 </label>
                 <input
                   type="email"
                   name="email"
+                  {...register("email")}
                   placeholder="you@example.com"
-                  className="py-2 px-4 bg-transparent  text-white border border-light-1 md:text-lg rounded md:rounded-md"
+                  className="py-2 px-4 bg-transparent  text-white border border-light-1  rounded md:rounded-md"
                 />
+                {errors.email && <InputError error={errors.email.message} />}
               </div>
               <div className="flex flex-col space-y-2">
-                <label htmlFor="fullName" className="text-white md:text-lg">
+                <label htmlFor="fullName" className="text-white ">
                   Password:
                 </label>
                 <input
                   type="password"
                   name="password"
-                  className="py-2 px-4 bg-transparent  text-white border border-light-1 md:text-lg rounded md:rounded-md"
+                  {...register("password")}
+                  className="py-2 px-4 bg-transparent  text-white border border-light-1  rounded md:rounded-md"
                 />
+                {errors.password && (
+                  <InputError error={errors.password.message} />
+                )}
               </div>
               <div className="flex flex-col space-y-2">
-                <label htmlFor="fullName" className="text-white md:text-lg">
+                <label htmlFor="fullName" className="text-white ">
                   Password Confirm:
                 </label>
                 <input
                   type="password"
                   name="passwordConfirm"
-                  className="py-2 px-4 bg-transparent  text-white border border-light-1 md:text-lg rounded md:rounded-md"
+                  {...register("passwordConfirm")}
+                  className="py-2 px-4 bg-transparent  text-white border border-light-1  rounded md:rounded-md"
                 />
+                {errors.passwordConfirm && (
+                  <InputError error={errors.passwordConfirm.message} />
+                )}
               </div>
 
               <button
                 type="submit"
-                className="w-full bg-light-3 text-white py-2 px-4 rounded-md hover:bg-black focus:outline-none focus:bg-black"
+                className="w-full bg-light-3 text-white py-2 px-4 rounded-md hover:bg-black focus:outline-none hover:border hover:border-white focus:ring ring-white"
               >
-                Register
+                {isLoading ? "Loading..." : "Register"}
               </button>
               <p className="text-sm text-white text-center">
                 Already have an account?{" "}
