ccdf4dfbcc9fc6094f7f6e11131bedc07ac441cb
lebohangmdm <lebohangmdm@gmail.com>
2024-06-21 10:24:05 +0200

    fix the hero page

--- clients/src/components/Hero.jsx
+++ clients/src/components/Hero.jsx
@@ -1,39 +1,76 @@
-// import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-// import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import { useCallback, useEffect, useState } from "react";
 
 const Hero = () => {
+  const slides = [
+    {
+      url: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
+    },
+
+    {
+      url: "https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = useCallback(() => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  }, [slides, currentIndex]);
+
+  const nextSlide = useCallback(() => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  }, [slides, currentIndex]);
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  // Auto-rotate slides every 5 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [nextSlide]);
+
   return (
-    <section className="-z-10 relative  w-full h-screen overflow-hidden border-white border">
-      <div className="">
-        <video
-          autoPlay
-          muted
-          className="absolute top-1/2 left-1/2 w-auto min-w-full min-h-full max-w-none transform -translate-x-1/2 -translate-y-1/2"
-        >
-          <source src="src\assets\trigger.mp4" type="video/mp4"></source>
-        </video>
-        <div className="z-10 text-light-1 absolute bottom-0 px-12">
-          <h1 className="text-5xl font-semibold uppercase">Trigger</h1>
-          <p className="mb-3 word-spacing-[-5px]">
-            Drama
-            <strong> &#183;</strong> 2h 31min{" "}
-            <span className="text-sm">TV-Ma</span>
-          </p>
-          <p className="max-w-sm leading-normal mb-4">
-            An FBI agent must find who killed the president, inside the
-            organization
-          </p>
-          {/* <Link
-            to={"/movies/1"}
-            className="bg-light-1 text-black uppercase tracking-wide text-base font-semibold transform duration-300 ease-out py-3 px-8 rounded-md hover:bg-white cursor-pointer"
+    <section className=" relative group w-full h-height-dvh">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full bg-center bg-cover duration-500"
+      ></div>
+      {/* Left Arrow */}
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-gray-500/20 text-white cursor-pointer">
+        <ChevronLeftIcon className="w-9 h-9 text-white" onClick={prevSlide} />
+      </div>
+      {/* Right Arrow */}
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-gray-800/60 text-white cursor-pointer">
+        <ChevronRightIcon className="w-9 h-9 text-white " onClick={nextSlide} />
+      </div>
+      <div className="absolute bottom-2 right-1/2 left-1/2 flex justify-center gap-2 ">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className={`text-xl text-white cursor-default ${slideIndex === currentIndex ? "text-white" : "text-gray-700"}`}
           >
-            <PlayArrowIcon /> Play
-          </Link> */}
-          <Button variant="contained" color="primary">
-            Play
-          </Button>
-        </div>
+            &#9679;
+          </div>
+        ))}
       </div>
     </section>
   );
